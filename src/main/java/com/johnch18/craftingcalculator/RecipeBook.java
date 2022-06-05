5592557c00dac2fd0e30f631611a03afcf1e08db
Charles Johnson <johnch18@isu.edu>
2022-06-04 20:07:03 -0600

    Adding serialization/deserialization.

--- src/main/java/com/johnch18/craftingcalculator/RecipeBook.java
+++ src/main/java/com/johnch18/craftingcalculator/RecipeBook.java
@@ -1,13 +1,46 @@
 package com.johnch18.craftingcalculator;
 
 import com.johnch18.craftingcalculator.exceptions.CCInvalidIngredientString;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RecipeBook {
 
     private List<Recipe> recipes;
+
+    public static RecipeBook loadBookFromFile(String fileName) throws IOException {
+        RecipeBook result = new RecipeBook();
+        JSONObject object = new JSONObject(readFile(fileName));
+        loadComponents(result, object);
+        return result;
+    }
+
+    private static void loadComponents(RecipeBook result, JSONObject object) {
+        JSONObject componentMap = (JSONObject) object.get("components");
+    }
+
+    private static String readFile(String fileName) throws IOException {
+        String everything;
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public RecipeBook() {
         recipes = new ArrayList<>();
