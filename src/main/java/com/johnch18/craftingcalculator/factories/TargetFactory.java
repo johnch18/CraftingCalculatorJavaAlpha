d029ced8843bd0850a4955c6670682a45c99356a
Charles Johnson <johnch18@isu.edu>
2022-06-15 11:48:48 -0600

    meme

--- src/main/java/com/johnch18/craftingcalculator/factories/TargetFactory.java
+++ src/main/java/com/johnch18/craftingcalculator/factories/TargetFactory.java
@@ -4,17 +4,26 @@ import com.johnch18.craftingcalculator.impl.Target;
 import com.johnch18.craftingcalculator.intr.ITarget;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TargetFactory {
+
+    private final static List<ITarget> targetList = new ArrayList<>();
 
     public static ITarget deserialize(JSONObject json) {
         String name = json.getString("name");
         int metadata = json.optInt("metadata", 0);
         String type = json.optString("type", "null");
+        ITarget target;
         switch (type) {
             default: {
-                return new Target(name, metadata);
+                target = new Target(name, metadata);
             }
         }
+        if (targetList.contains(target))
+            return targetList.get(targetList.indexOf(targetList));
+        return target;
     }
 
 }
